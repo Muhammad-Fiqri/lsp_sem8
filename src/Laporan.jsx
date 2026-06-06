219e3d8a0e5e81eaf775e31b99b153e2503831a5
Muhammad Fiqri <mfiqrischool101@gmail.com>
2026-06-06 16:38:48 +0700

    feature: print report page with react-to-pdf

--- src/Laporan.jsx
+++ src/Laporan.jsx
@@ -1,4 +1,5 @@
 import { useState, useRef, useEffect } from "react";
+import { useReactToPrint } from "react-to-print";
 import './stylesheet/Laporan.css'
 
 export default function Laporan() {
@@ -28,73 +29,79 @@ export default function Laporan() {
         })
     },[])
 
+    const contentRef = useRef(null);
+    const reactToPrintFn = useReactToPrint({ contentRef });
+
     return(
         <div className="laporan">
-            <div className="total-stok-masuk">
-            <h3>Total Stok Masuk</h3>
-            <hr />
-            <table>
-                <thead>
-                    <tr>
-                    <th>No</th>
-                    <th>Nama Barang</th>
-                    <th>Tanggal</th>
-                    <th>Jumlah</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    { 
-                        laporanData != null ?
-                        laporanData.total_stok_masuk.map((row, i = 0) => {
-                        i += 1;
-                        
-                        return(
-                            <tr key={i}>
-                            <td>{i}</td>
-                            <td>{row.name_products}</td>
-                            <td>{row.date.split('T')[0] /*remove timezone*/}</td> 
-                            <td>{row.amount}</td>
-                            </tr>
-                        )
-                        })
-                        :
-                        <tr></tr>
-                    }
-                </tbody>
-            </table>
-            </div>
-            <div className="total-stok-keluar">
-                <h3>Total Stok Keluar</h3>
+            <button className="print-report-button" onClick={reactToPrintFn}>Cetak Laporan 🖨️</button>
+            <div id="laporan-page" ref={contentRef}>
+                <div className="total-stok-masuk">
+                <h3>Total Stok Masuk</h3>
                 <hr />
                 <table>
-                <thead>
-                    <tr>
-                    <th>No</th>
-                    <th>Nama Barang</th>
-                    <th>Tanggal</th>
-                    <th>Jumlah</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    { 
-                        laporanData != null ?
-                        laporanData.total_stok_keluar.map((row, i = 0) => {
-                        i += 1;
-                        
-                        return(
-                            <tr key={i}>
-                            <td>{i}</td>
-                            <td>{row.name_products}</td>
-                            <td>{row.date.split('T')[0] /*remove timezone*/}</td> 
-                            <td>{row.amount}</td>
-                            </tr>
-                        )
-                        })
-                        :
-                        <tr></tr>
-                    }
-                </tbody>
+                    <thead>
+                        <tr>
+                        <th>No</th>
+                        <th>Nama Barang</th>
+                        <th>Tanggal</th>
+                        <th>Jumlah</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        { 
+                            laporanData != null ?
+                            laporanData.total_stok_masuk.map((row, i = 0) => {
+                            i += 1;
+                            
+                            return(
+                                <tr key={i}>
+                                <td>{i}</td>
+                                <td>{row.name_products}</td>
+                                <td>{row.date.split('T')[0] /*remove timezone*/}</td> 
+                                <td>{row.amount}</td>
+                                </tr>
+                            )
+                            })
+                            :
+                            <tr></tr>
+                        }
+                    </tbody>
                 </table>
+                </div>
+                <div className="total-stok-keluar">
+                    <h3>Total Stok Keluar</h3>
+                    <hr />
+                    <table>
+                    <thead>
+                        <tr>
+                        <th>No</th>
+                        <th>Nama Barang</th>
+                        <th>Tanggal</th>
+                        <th>Jumlah</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        { 
+                            laporanData != null ?
+                            laporanData.total_stok_keluar.map((row, i = 0) => {
+                            i += 1;
+                            
+                            return(
+                                <tr key={i}>
+                                <td>{i}</td>
+                                <td>{row.name_products}</td>
+                                <td>{row.date.split('T')[0] /*remove timezone*/}</td> 
+                                <td>{row.amount}</td>
+                                </tr>
+                            )
+                            })
+                            :
+                            <tr></tr>
+                        }
+                    </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     )
